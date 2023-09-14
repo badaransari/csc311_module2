38daf42e909bbf1fc16799d070c3fdcf49c3555e
Guest <Guest@badars-mbp>
2023-09-14 01:25:28 -0400

    added constructor, getter and setter

---
+++ src/main/java/com/mycompany/studentlst/Student.java
@@ -0,0 +1,33 @@
+
+package com.mycompany.studentlst;
+
+/**
+ *
+ * @author Badar
+ */
+
+public class Student {
+    
+    private String name;
+
+    // Constructor
+    public Student(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
+
+    
+    
+    
+    
